9e10454cedaab729073eebf74895d2f45d039493
Luis Quintanilla <lqdev@outlook.com>
2021-02-09 20:30:02 -0500

    Fixed alignment

--- Views/Partials.fs
+++ Views/Partials.fs
@@ -11,7 +11,6 @@ module PartialViews
 
     let recentPostsView (posts:Post array) = 
         div [] [
-            h2 [] [Text "Recent Posts"]
             ul [_class "list-group"] [
                 for post in posts do
                     li [_class "list-group-item"] [
@@ -24,6 +23,7 @@ module PartialViews
 
     let homeView content = 
         div [_class "mr-auto"] [
+            h2 [] [Text "Recent Posts"]
             rawText content    
         ]
 
@@ -41,37 +41,33 @@ module PartialViews
         let nextPage = currentPage + 1
         let previousPage = currentPage - 1
         div [_class "mr-auto"] [
-            div [_class "mr-auto"] [
-                ul [_class "list-group"] [
-                    for post in posts do
-                        li [_class "list-group-item"] [
-                            a [_href (sprintf "/posts/%s.html" post.FileName)] [
-                                h3 [] [Text post.Metadata.Title]
-                            ]
-                        ]
-                ]
-            ]
 
-            if(lastPage > 1) then
-                if(currentPage = 1) then
-                    div [_class "btn-group justify-content-end mr-auto"] [
-                        a [_href "/posts/1"; _class "btn btn-secondary"] [Text "First"]
-                        a [_href (sprintf "/posts/%i" nextPage); _class "btn btn-secondary"] [Text "Next"]
-                        a [_href (sprintf "/posts/%i" lastPage); _class "btn btn-secondary"] [Text "Last"]
-                    ]
-                else if(currentPage = lastPage) then
-                    div [_class "btn-group justify-content-end mr-auto"] [
-                        a [_href "/posts/1"; _class "btn btn-secondary"] [Text "First"]
-                        a [_href (sprintf "/posts/%i" previousPage ); _class "btn btn-secondary"] [Text "Back"]
-                        a [_href (sprintf "/posts/%i" lastPage); _class "btn btn-secondary"] [Text "Last"]
-                    ]
-                else if(currentPage > 1) then
-                    div [_class "btn-group justify-content-end mr-auto"] [
-                        a [_href "/posts/1"; _class "btn btn-secondary"] [Text "First"]
-                        a [_href (sprintf "/posts/%i" previousPage); _class "btn btn-secondary"] [Text "Back"]
-                        a [_href (sprintf "/posts/%i" nextPage); _class "btn btn-secondary"] [Text "Next"]
-                        a [_href (sprintf "/posts/%i" lastPage); _class "btn btn-secondary"] [Text "Last"] 
-                    ]
+            div [] [
+                recentPostsView posts
+            ]
+            
+            div [_class "mt-2 text-center"] [
+                if(lastPage > 1) then
+                    if(currentPage = 1) then
+                        div [_class "btn-group"] [
+                            a [_href "/posts/1"; _class "btn btn-secondary"] [Text "First"]
+                            a [_href (sprintf "/posts/%i" nextPage); _class "btn btn-secondary"] [Text "Next"]
+                            a [_href (sprintf "/posts/%i" lastPage); _class "btn btn-secondary"] [Text "Last"]
+                        ]
+                    else if(currentPage = lastPage) then
+                        div [_class "btn-group"] [
+                            a [_href "/posts/1"; _class "btn btn-secondary"] [Text "First"]
+                            a [_href (sprintf "/posts/%i" previousPage ); _class "btn btn-secondary"] [Text "Back"]
+                            a [_href (sprintf "/posts/%i" lastPage); _class "btn btn-secondary"] [Text "Last"]
+                        ]
+                    else if(currentPage > 1) then
+                        div [_class "btn-group"] [
+                            a [_href "/posts/1"; _class "btn btn-secondary"] [Text "First"]
+                            a [_href (sprintf "/posts/%i" previousPage); _class "btn btn-secondary"] [Text "Back"]
+                            a [_href (sprintf "/posts/%i" nextPage); _class "btn btn-secondary"] [Text "Next"]
+                            a [_href (sprintf "/posts/%i" lastPage); _class "btn btn-secondary"] [Text "Last"] 
+                        ]
+            ]
         ]
 
     let eventView (events:Event array) = 
