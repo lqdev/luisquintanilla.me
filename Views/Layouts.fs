cee312de78f725ae419c81b7706d4ec01f328ded
Luis Quintanilla <lqdev@outlook.com>
2021-07-27 22:37:57 -0400

    Fixed RSS link icon formatting

--- Views/Layouts.fs
+++ Views/Layouts.fs
@@ -5,7 +5,7 @@ module Layouts
     let defaultNavBar = 
         nav [_class "navbar navbar-expand-md navbar-dark fixed-top bg-dark"] [
             a [_class "navbar-brand"; _href "#"] [ 
-                img [_src "/avatar.png"; _height "30"; _width "30"; _class "d-inline-block align-top rounded-circle"; _style "margin-right:5px"]
+                img [_src "/avatar.png"; _height "32"; _width "32"; _class "d-inline-block align-top rounded-circle"; _style "margin-right:5px"]
                 Text "Luis Quintanilla" ]
             button [
                 _type "button"
@@ -36,7 +36,16 @@ module Layouts
                     ]                                        
                 ]
                 a [_href "/feed.rss"] [
-                    img [_src "/css/bootstrap-icons-1.5.0/rss.svg"; _width "32";_height "32"; _color "secondary"]
+                    tag "svg" [
+                        _class "bi bi-rss text-secondary" 
+                        attr "fill" "currentColor"
+                        attr "viewBox" "0 0 16 16"
+                        _height "32"
+                        _width "32"] [
+                        
+                        tag "path" [attr "d" "M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"] []
+                        tag "path" [attr "d" "M5.5 12a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-3-8.5a1 1 0 0 1 1-1c5.523 0 10 4.477 10 10a1 1 0 1 1-2 0 8 8 0 0 0-8-8 1 1 0 0 1-1-1zm0 4a1 1 0 0 1 1-1 6 6 0 0 1 6 6 1 1 0 1 1-2 0 4 4 0 0 0-4-4 1 1 0 0 1-1-1z"] []
+                    ]
                 ]
             ]
         ]
