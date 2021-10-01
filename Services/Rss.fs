771fd2e148e54b111f88e3309034253a519ac2c4
Luis Quintanilla <lqdev@outlook.com>
2021-09-30 21:27:34 -0400

    Updated feed locations and generated main feed

--- Services/Rss.fs
+++ Services/Rss.fs
@@ -8,11 +8,11 @@ module RssService
 
     let title = "Luis Quintanilla"
     let link = "https://www.luisquintanilla.me"
-    let description = "Luis Quintanilla's personal website"
+    let description = "Luis Quintanilla's blog"
     // let lastPubDate = "02/09/2021"
     let language = "en"
 
-    let entryXml (entry:Post) =
+    let blogEntryXml (entry:Post) =
         
         let filePath = sprintf "%s.html" entry.FileName
         let url = sprintf "https://www.luisquintanilla.me/posts/%s" filePath
@@ -24,7 +24,18 @@ module RssService
             XElement(XName.Get "guid", url),
             XElement(XName.Get "pubDate", (DateTime.Parse(entry.Metadata.Date).ToShortDateString())))    
 
-    let channelXml (lastPubDate:string) = 
+    let feedEntryXml (entry:Post) =
+
+        let filePath = sprintf "%s.html" entry.FileName
+        let url = sprintf "https://www.luisquintanilla.me/feed/%s" filePath
+        
+        XElement(XName.Get "item",
+            XElement(XName.Get "title", entry.Metadata.Title),
+            XElement(XName.Get "link", url),
+            XElement(XName.Get "guid", url),
+            XElement(XName.Get "pubDate", (DateTime.Parse(entry.Metadata.Date).ToShortDateString())))
+
+    let blogChannelXml (lastPubDate:string) = 
         XElement(XName.Get "rss",
             XAttribute(XName.Get "version","2.0"),
             XElement(XName.Get "channel",
@@ -33,11 +44,29 @@ module RssService
                 XElement(XName.Get "description", description),
                 XElement(XName.Get "lastPubDate", lastPubDate),
                 XElement(XName.Get "language", language)))
-                
-    let generateRss (posts:Post array) = 
+
+    let feedChannelXml (title:string) (description:string) (lastPubDate:string) = 
+        XElement(XName.Get "rss",
+            XAttribute(XName.Get "version","2.0"),
+            XElement(XName.Get "channel",
+                XElement(XName.Get "title", title),
+                XElement(XName.Get "link", link),
+                XElement(XName.Get "description", description),
+                XElement(XName.Get "lastPubDate", lastPubDate),
+                XElement(XName.Get "language", language)))   
+             
+    let generateBlogRss (posts:Post array) = 
         let latestPost = posts |> Array.sortByDescending(fun post -> DateTime.Parse(post.Metadata.Date)) |> Array.head 
-        let entries = posts |> Array.map(entryXml)
-        let channel = channelXml (DateTime.Parse(latestPost.Metadata.Date).ToShortDateString())
+        let entries = posts |> Array.map(blogEntryXml)
+        let channel = blogChannelXml (DateTime.Parse(latestPost.Metadata.Date).ToShortDateString())
         
         channel.Descendants(XName.Get "channel").First().Add(entries)
         channel
+
+    let generateMainFeedRss (posts:Post array) =
+        let latestPost = posts |> Array.sortByDescending(fun post -> DateTime.Parse(post.Metadata.Date)) |> Array.head 
+        let entries = posts |> Array.map(feedEntryXml)
+        let channel = feedChannelXml "Luis Quintanilla" "Main Feed" (DateTime.Parse(latestPost.Metadata.Date).ToShortDateString())
+        
+        channel.Descendants(XName.Get "channel").First().Add(entries)
+        channel 
