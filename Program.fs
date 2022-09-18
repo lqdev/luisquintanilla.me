1aad6a44a7084435c3346e6e4d888448471fa1ad
Luis Quintanilla <lqdev@outlook.com>
2022-09-17 19:38:49 -0500

    Test datetime

--- Program.fs
+++ Program.fs
@@ -113,7 +113,7 @@ let main argv =
         responses
         |> Array.filter(fun x -> 
             let currentDateTime = DateTimeOffset(DateTime.Now).ToOffset(TimeSpan(-4,0,0))
-            let updatedDateTime = DateTimeOffset(DateTime.Parse(x.Metadata.DateUpdated).AddMinutes(60)).ToOffset(TimeSpan(-4,0,0))
+            let updatedDateTime = DateTimeOffset(DateTime.SpecifyKind(DateTime.Parse(x.Metadata.DateUpdated).AddMinutes(60),DateTimeKind.Local).ToOffset(TimeSpan(-4,0,0))
             printfn $"Current: {currentDateTime}"
             printfn $"Updated: {updatedDateTime}"
             currentDateTime < updatedDateTime)
