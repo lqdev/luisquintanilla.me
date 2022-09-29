018d26538e0f389a2db1e729bb22228ff51ae55b
Luis Quintanilla <lqdev@outlook.com>
2022-09-29 11:54:16 -0500

    Header URL redirect to about

--- Views/Partials.fs
+++ Views/Partials.fs
@@ -251,7 +251,7 @@ let cardHeader (date:string) =
 
     div [_class "card-header u-author h-card"] [
         img [_src "/avatar.png"; _height "32"; _width "32"; _class "d-inline-block align-top rounded-circle u-photo"; _style "margin-right:5px"; attr "loading" "lazy"]
-        a [ _href "http://lqdev.me"; _class "p-name"] [Text "lqdev"]
+        a [ _href "http://lqdev.me/about"; _class "p-name"] [Text "lqdev"]
         span [_class "bi bi-shield-fill-check"; _style "margin-right:5px;margin-left:5px;color:green"] []
         span [_class "float-right"] [
             time [_class "dt-published"; _datetime date] [Text $"{dt.ToShortDateString()}"]
