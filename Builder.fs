d6d48687c056d324d2e1bd435397d3b3c9aa137a
Luis Quintanilla <lqdev@outlook.com>
2023-08-16 22:01:39 -0400

    Add builder debug

--- Builder.fs
+++ Builder.fs
@@ -65,7 +65,9 @@ module Builder
 
         let recentFeedPost = 
             feedPosts
-            |> Array.sortByDescending(fun x-> DateTime.Parse(x.Metadata.Date))
+            |> Array.sortByDescending(fun x-> 
+                printfn $"{x.Metadata.Title}{x.Metadata.Date}"
+                DateTime.Parse(x.Metadata.Date))
             |> Array.head
 
 
