66c59b108355c72057eb8df7e7cb7374c2dbabc2
Luis Quintanilla <lqdev@outlook.com>
2024-01-06 12:40:49 -0500

    Update avatar

--- Views/Layouts.fs
+++ Views/Layouts.fs
@@ -5,7 +5,7 @@ module Layouts
     let defaultNavBar = 
         nav [_class "navbar navbar-expand-md navbar-dark fixed-top bg-dark"] [
             a [_class "navbar-brand"; _href "/"] [ 
-                img [_src "/art-profile.png"; _height "32"; _width "32"; _class "d-inline-block align-top rounded-circle"; _style "margin-right:5px"; attr "loading" "lazy"]
+                img [_src "/avatar.png"; _height "32"; _width "32"; _class "d-inline-block align-top rounded-circle"; _style "margin-right:5px"; attr "loading" "lazy"]
                 Text "Luis Quintanilla" ]
             button [
                 _type "button"
