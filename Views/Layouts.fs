c21d2d2ed026352785d0e62d3980b18caa3d79ed
Luis Quintanilla <lqdev@outlook.com>
2024-01-20 19:49:45 -0500

    Reorganize nav bar

--- Views/Layouts.fs
+++ Views/Layouts.fs
@@ -66,17 +66,15 @@ module Layouts
                             ] [
                                 a [_class "dropdown-item"; _href "/feed"] [Text "Main"]
                                 a [_class "dropdown-item"; _href "/feed/responses"] [Text "Responses"]
-                                a [_class "dropdown-item"; _href "/posts/1"] [ Text "Blog" ]                                
+                                a [_class "dropdown-item"; _href "/posts/1"] [ Text "Blog" ]
+                                div [_class "dropdown-divider"] []
+                                a [_class "dropdown-item"; _href "/subscribe"] [ Text "Subscribe" ]
                                 div [_class "dropdown-divider"] []
                                 a [_class "dropdown-item"; _href "/feed/blogroll"] [Text "Blogroll"]
                                 a [_class "dropdown-item"; _href "/feed/podroll"] [Text "Podroll"]
                         ]
                     ]
                     
-                    li [_class "nav-item"] [
-                        a [_class "nav-link"; _href "/subscribe"] [ Text "Subscribe" ]
-                    ]
-
                     //Collections dropdown
                     li [_class "nav-item dropdown"] [
                         a [
@@ -126,23 +124,6 @@ module Layouts
                         a [_class "nav-link"; _href "/events"] [ Text "Events" ]
                     ]                    
                 ]
-
-                span [
-                    _href "#"
-                    _onclick "switchTheme()"
-                    ] [
-                    tag "svg" [
-                        _class "bi bi-brightness-high-fill" 
-                        attr "fill" "currentColor"
-                        attr "viewBox" "0 0 16 16"
-                        _height "32"
-                        _width "32"] [
-                        
-                        tag "path" [attr "d" "M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"] []
-
-                    ]
-                ]
-
 
                 a [_href "/subscribe"] [
                     tag "svg" [
